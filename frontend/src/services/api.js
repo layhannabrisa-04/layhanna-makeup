b1b347f7fc56f42cb5e89639f50cd5ab09a226f7
OdilioCarneiro <odilionetocarneironogueira@gmail.com>
2026-06-16 11:58:58 -0300

    fix: update baseURL configuration to use environment variable for API endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', // Conecta direto com as suas rotas do Express
+  // Se existir a variável do Render (nuvem), ele usa ela. 
+  // Se não existir (no seu PC), ele usa o localhost!
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000'
 });
 
 export default api;
